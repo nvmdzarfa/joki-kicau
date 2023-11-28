a8343175e529ab36ceb1e8dc514b3180c8a1c89e
nvmdzarfa <geizarfa.01@gmail.com>
2023-11-28 16:52:28 +0700

    fix sizing #contact, Add Feature but not capable for searching 'search box & form'

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// Toggle Class Active
+// Toggle Class Active for Hamburger Menu
 const navbarNav = document.querySelector(".navbar-nav");
 
 // if hamburger menu clicked
@@ -6,10 +6,26 @@ document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
-// clicked outside sidebar for hide nav
-const hamburger = document.querySelector("#hamburger-menu");
+// Toggle Class Active for Search Form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// clicked outside element
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button");
+
 document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
